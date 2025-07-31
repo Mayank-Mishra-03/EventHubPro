e6ea2235eca9ccc142d72a3abf500b58ab959258
Mayank-Mishra-03 <mayank0303work@gmail.com>
2025-07-31 20:57:32 +0530

    Add CORS support to the backend

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import userRoutes from './routes/user.route.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 const app = express();
 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(cookieParser());
+app.use(cors());
 
 
 app.use('/api/v1/users', userRoutes);
